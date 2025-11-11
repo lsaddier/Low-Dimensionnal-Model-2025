2f910f03cd81eca88e65b73a2164f49026b43a35
lsaddier <143815934+lsaddier@users.noreply.github.com>
2025-11-11 17:07:43 +0100

    Replot Fig.3

--- fct_BoxModel.py
+++ fct_BoxModel.py
@@ -67,7 +67,9 @@ def PICO(Td,Sd, nbox, C, gammaT, Ac, frac, depth):
 
 # compute AABW flux [m^3/s]
 def compute_DSW(T, S, C2, T0, S0):
-    return np.max(np.array([0,C2*(EOS(T,S)-EOS(T0,S0))])) #Ensure non-negative flux
+    return C2*(EOS(T,S)-EOS(T0,S0)) #Ensure non-negative flux
+    #return np.max(np.array([0,C2*(EOS(T,S)-EOS(T0,S0))])) #Ensure non-negative flux
+
 
 # compute water column stability [kg/m^3] # work only in diffusive mode
 def compute_Sigma(X):
